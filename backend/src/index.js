3c87dbbeeff0d695f586d83bfadb859d4b426378
codax0421 <59133030+codax0421@users.noreply.github.com>
2022-12-25 21:08:47 +0800

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 
 mongo.connect();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 server.listen({ port }, () => {
   console.log(`The server is up on port ${port}!`);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,27 +6,29 @@ import {
   ApolloProvider,
   split,
   HttpLink,
-} from '@apollo/client';
-import { getMainDefinition } from '@apollo/client/utilities';
-import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
-import { createClient } from 'graphql-ws';
+} from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
 import "./index.css";
 import App from "./containers/App";
-import { ChatProvider } from "./containers/hooks/useChat"
+import { ChatProvider } from "./containers/hooks/useChat";
 import reportWebVitals from "./reportWebVitals";
 import "antd";
 // import "antd/dist/antd.css";
 
 // Create an http link:
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/',
+  uri: "http://localhost:80/",
 });
 
 // Create a WebSocket link:
-const wsLink = new GraphQLWsLink(createClient({
-  url: 'ws://localhost:4000/',
-  options: { reconnect: true },
-}));
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:80/",
+    options: { reconnect: true },
+  })
+);
 
 // using the ability to split links, you can send data to each link
 // depending on what kind of operation is being sent
@@ -35,12 +37,12 @@ const link = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
     return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
     );
   },
   wsLink,
-  httpLink,
+  httpLink
 );
 
 const client = new ApolloClient({
@@ -52,8 +54,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <ChatProvider><App /></ChatProvider>
-        {/* <App /> */}
+      <ChatProvider>
+        <App />
+      </ChatProvider>
+      {/* <App /> */}
     </ApolloProvider>
   </React.StrictMode>
 );
