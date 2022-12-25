08c03fc06ee5cb1c4845e5483bd24e7b56365d80
codax0421 <59133030+codax0421@users.noreply.github.com>
2022-12-25 19:24:02 +0800

    Delete frontend directory

--- frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: `http://localhost:80/`,
-});
-
-export default instance;
-
-// instance.get('/hi').then((data) => console.log(data));
