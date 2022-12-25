08f63345ede54f4fcb30283b662978a426a738a8
codax0421 <59133030+codax0421@users.noreply.github.com>
2022-12-25 18:41:37 +0800

    cahges

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,18 @@
-import express from 'express';
-import mongoose from "mongoose"
-import cors from 'cors';
-import db from "../mongodb.js"
-import routes from './routes/index.js';
-import bodyParser from "body-parser"
-import 'dotenv-defaults'
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import db from "../mongodb.js";
+import routes from "./routes/index.js";
+import bodyParser from "body-parser";
+import "dotenv-defaults";
 
-require('dotenv-defaults').config()
+require("dotenv-defaults").config();
 const app = express();
 const port = process.env.PORT || 4000;
-app.listen(port, () =>
- console.log(`Example app listening on port ${port}!`),
-);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 db.connect();
 app.use(cors());
-app.use('/', routes);
+app.use("/", routes);
 app.use(express.json());
 
 // app.post("/card", (req,res)=>{
@@ -22,5 +20,4 @@ app.use(express.json());
 //    let name ;
 //    let subject;
 
- 
 // });
